941080d779f25d07a4a2478143b8f737350e542b
unknown <202080480@psu.palawan.edu.ph>
2022-10-12 04:50:28 +0800

    new changes

---
+++ logApp-scaling-octo/config/config.php
@@ -0,0 +1,6 @@
+<?php
+	define('ROOT_URL', '');
+	define('DB_HOST', 'sql6.freesqldatabase.com');
+	define('DB_USER', 'sql6525747');
+	define('DB_PASS', 'ci95qnZQAr');
+	define('DB_NAME', 'sql6525747');
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'razcelett/logapp-rancap',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '6bc01572c991946996d9685f0429016bcd2a9f2f',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'razcelett/logapp-rancap' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '6bc01572c991946996d9685f0429016bcd2a9f2f',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
